a5be89162d9d3ee0d71df2cecf2d366f9e939517
Jason Madden <jamadden@gmail.com>
2017-07-05 16:52:39 -0500

    Back to development: 1.1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name="z3c.recipe.sphinxdoc",
-    version='1.1.0',
+    version='1.1.1.dev0',
     author='Paul Carduner, Zope Foundation and Contributors',
     author_email="zope-dev@zope.org",
     description="Use Sphinx to build documentation for zope.org.",
